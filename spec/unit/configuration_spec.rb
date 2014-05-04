e617094fa7782a845e7e60624464d04083944819
Piotr Murach <pmurach@gmail.com>
2014-05-04 19:09:48 +0100

    Add config tests.

--- spec/unit/configuration_spec.rb
+++ spec/unit/configuration_spec.rb
@@ -7,15 +7,34 @@ describe Supervision::Configuration do
 
   subject(:config) { described_class.new }
 
-  it { expect(config.max_failures).to eql(5) }
-
-  it { expect(config.call_timeout).to eql(0.01) }
-
-  it { expect(config.reset_timeout).to eql(0.1) }
-
   it "fails fast with unknown config option" do
     expect {
       object.new max_fail: 2
     }.to raise_error(Supervision::InvalidParameterError)
+  end
+
+  context 'when default' do
+    it { expect(config.max_failures).to eql(5) }
+
+    it { expect(config.call_timeout).to eql(0.01) }
+
+    it { expect(config.reset_timeout).to eql(0.1) }
+  end
+
+  context 'when setting' do
+    it "sets maximum failures" do
+      config.max_failures(3)
+      expect(config.max_failures).to eq(3)
+    end
+
+    it "sets call timeout" do
+      config.call_timeout(1.sec)
+      expect(config.call_timeout).to eq(1.sec)
+    end
+
+    it "sets reset timeout" do
+      config.reset_timeout(10.sec)
+      expect(config.reset_timeout).to eq(10.sec)
+    end
   end
 end
