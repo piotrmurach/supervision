a3826feecf5ac87d171d4cf418870095e6cc1fea
Piotr Murach <pmurach@gmail.com>
2014-05-05 22:35:37 +0100

    Add simple statistics to monitor.

--- lib/supervision/circuit_monitor.rb
+++ lib/supervision/circuit_monitor.rb
@@ -4,10 +4,55 @@ module Supervision
   # A class responsible for recording circuit performance
   class CircuitMonitor
 
+    attr_reader :times_opened
+
+    # Timestamp for the last circuit open state
+    #
+    # @api public
+    attr_reader :last_opened
+
     def initialize
+      @total_failed_calls  = Counter.new
+      @total_success_calls = Counter.new
+      @total_calls         = Counter.new
+      @state_transitions   = Counter.new
     end
 
-    def alert(type)
+    def total_calls
+      @total_calls.value
     end
-  end
+
+    def total_success_calls
+      @total_success_calls.value
+    end
+
+    def total_failed_calls
+      @total_failed_calls.value
+    end
+
+    def record_success
+      @total_success_calls.increment
+      @total_calls.increment
+    end
+
+    def record_failure
+      @total_failed_calls.increment
+      @total_calls.increment
+    end
+
+    def measure(type)
+
+    end
+
+    # Reset the circuit statistics
+    #
+    # @return [nil]
+    #
+    # @api public
+    def reset
+      total_calls.clear
+      total_success_calls.clear
+      total_failed_calls.clear
+    end
+  end # CircuitMonitor
 end # Supervision
--- spec/unit/circuit_monitor_spec.rb
+++ spec/unit/circuit_monitor_spec.rb
@@ -3,4 +3,25 @@
 require "spec_helper"
 
 describe Supervision::CircuitMonitor do
+  let(:object) { described_class }
+
+  subject(:monitor) { object.new }
+
+  describe "#record_success" do
+    it "records success" do
+      monitor.record_success
+      expect(monitor.total_success_calls).to eq(1)
+      expect(monitor.total_failed_calls).to eq(0)
+      expect(monitor.total_calls).to eq(1)
+    end
+  end
+
+  describe "#record_failure" do
+    it "records failure" do
+      monitor.record_failure
+      expect(monitor.total_success_calls).to eq(0)
+      expect(monitor.total_failed_calls).to eq(1)
+      expect(monitor.total_calls).to eq(1)
+    end
+  end
 end
